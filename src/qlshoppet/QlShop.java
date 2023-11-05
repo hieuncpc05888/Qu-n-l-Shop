ebb067ee516e3ce86488bcf13f23f9827568598c
hieuncpc05888 <139949388+hieuncpc05888@users.noreply.github.com>
2023-11-05 16:46:25 +0700

    init

--- src/qlshoppet/QlShop.java
+++ src/qlshoppet/QlShop.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package qlshop;
+package qlshoppet;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
